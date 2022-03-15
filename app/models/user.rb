15763a959b712775345e36bcc300ade34841db7b
kalu6334 <martin.lucas76334@gmail.com>
2022-03-15 14:22:12 +0100

    add validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+  validates :password, presence: true
+
+  has_many :scooters
+
+
 end
