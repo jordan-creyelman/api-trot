f2b1139f8bb296c37ca3f80731b786058596a7da
Nicoclos <91563310+Nicoclos@users.noreply.github.com>
2022-03-16 22:46:45 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+  validates :password, presence: true
+
+
+
+  private
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 
 end
